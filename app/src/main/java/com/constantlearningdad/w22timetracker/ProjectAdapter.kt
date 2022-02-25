93ea3890a95171af4af984f797639d1e760fcf32
JaretWright <jaret.wright@georgiancollege.ca>
2022-02-25 10:41:50 -0500

    Week 7 - Created an adapter class to connect the item_project to the list of projects returned from the viewmodel

---
+++ app/src/main/java/com/constantlearningdad/w22timetracker/ProjectAdapter.kt
@@ -0,0 +1,51 @@
+package com.constantlearningdad.w22timetracker
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ProjectAdapter (val context : Context,
+                      val projects : List<Project>
+                      ) : RecyclerView.Adapter<ProjectAdapter.ProjectViewHolder>(){
+
+    /**
+     * This class is used to allow us to connect/access the elements in the
+     * item_project layout file
+     */
+    inner class ProjectViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val projectTextView = itemView.findViewById<TextView>(R.id.projectTextView)
+        val descriptionTextView = itemView.findViewById<TextView>(R.id.descriptionTextView)
+    }
+
+    /**
+     * This connects (aka inflates) the individual ViewHolder (which is the link to the item_project.xml)
+     * with the RecyclerView
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProjectViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_project, parent, false)
+        return ProjectViewHolder(view)
+    }
+
+    /**
+     * This method will bind the viewHolder with a specific project object
+     */
+    override fun onBindViewHolder(viewHolder: ProjectViewHolder, position: Int) {
+        val project = projects[position]
+        with(viewHolder){
+            projectTextView.text = project.projectName
+            descriptionTextView.text = project.description
+        }
+    }
+
+    /**
+     * This method returns the number of Projects in the recycler view
+     */
+    override fun getItemCount(): Int {
+        return projects.size
+    }
+
+}
